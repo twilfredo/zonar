29a8b9dd107d1fd11128ff5f7455edb33e728a50
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2026-02-24 17:33:03 +1000

    blockgroups: check fs writepointer before using device
    If the filesystem has reported a writepointer for a sequential write
    required blockgroup, then use that instead of the device wrtitepointer,
    as the device writepointer can trail behind the in memory allocation
    pointer.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- src/znr_bg.c
+++ src/znr_bg.c
@@ -89,11 +89,13 @@ static int znr_bg_map_zones_to_blockgroups(struct znr_bg *blockgroups,
 		}
 
 		blockgroups[i].blkz_flags = blockgroups[i].zones[0]->type;
-		if (blockgroups[i].blkz_flags == BLK_ZONE_TYPE_SEQWRITE_REQ)
+		if (blockgroups[i].blkz_flags == BLK_ZONE_TYPE_SEQWRITE_REQ &&
+		    blockgroups[i].fs_flags == BG_FS_NO_WP)
 			blockgroups[i].wp_sector =
 				blockgroups[i].zones[0]->wp -
 				blockgroups[i].sector;
-		else
+
+		if (blockgroups[i].blkz_flags == BLK_ZONE_TYPE_CONVENTIONAL)
 			blockgroups[i].wp_sector = 0;
 	}
 
