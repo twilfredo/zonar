bec4883b1e57956a6d213ea51d99a64b53b9c5aa
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2026-02-25 11:11:28 +1000

    xfs: add support for getting the writepointer
    For XFS RG groups, add support to reading the writepointer using the
    XFS_IOC_RTGROUP_GEOMETRY ioctl. The two main benefits of this are;
    
    1. For non-zoned devices, the filesystem writepointer can be
       used and rendered on the GUI to display the sequential write
       information.
    
    2. For zoned devices, the delta between the XFS WP and the device zone WP
       can now be drawn, which is helpful for debugging under heavy IOs.
    
    To represent the filesystem writepointer and the device zone backing
    writepointer the following attributes are introduced to struct znr_bg:
    	- dev_zone_wp_sector: contains the device backing zone
    	  write-pointer, if ZNR_BG_HAS_DEV_ZONE_WP is set.
    	- fs_wp_sector, contains the filesystem provided write-pointer
    	  if ZNR_BG_HAS_FS_WP is set.
    
    A helper znr_bg_has_wp() is added such that the GUI can check if the
    blockgroup is sequential or not without having to check both of the
    flags above at all times. Also add a helper znr_bg_set_zone_wp() to
    allow setting the write pointer in a blockgroup for zoned devices
    (minor refactor).
    
    Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- src/znr_bg.h
+++ src/znr_bg.h
@@ -15,8 +15,17 @@ struct znr_blockgroup {
 	/* Number of sectors */
 	unsigned long nr_sectors;
 
-	/* Write pointer sector offset within this blockgroup */
-	unsigned long wp_sector;
+	/*
+	 * The device indicated write pointer sector offset within this
+	 * blockgroup. Valid if ZNR_BG_HAS_DEV_ZONE_WP is set in flags.
+	 */
+	unsigned long dev_zone_wp;
+
+	/*
+	 * The current write pointer offset within this blockgroup used by the
+	 * FS to issue writes. Valif if ZNR_BG_HAS_FS_WP is set in flags.
+	 */
+	unsigned long fs_wp;
 
 	unsigned int flags;
 
@@ -24,6 +33,44 @@ struct znr_blockgroup {
 	struct blk_zone **zones;
 	unsigned long nr_zones;
 };
+
+enum znr_bg_flags {
+	/*
+	 * Set for a blockgroup backed by a sequential write required zone of
+	 * a zoned device.
+	 */
+	ZNR_BG_HAS_DEV_ZONE_WP	= (1U << 0),
+	/* Set if the filesystem provides a write pointer. */
+	ZNR_BG_HAS_FS_WP	= (1U << 1),
+	ZNR_BG_MAPPING_INITIALIZED = (1U << 2),
+};
+
+/*
+ * Helper function for zone backed blockgroups to check if the zone is
+ * fully written. Must only be called for zone backed blockgroups.
+ */
+static inline bool znr_bg_dev_zone_full(struct znr_blockgroup *bg)
+{
+	if (!bg->nr_zones || !bg->zones)
+		return false;
+
+	return bg->zones[0]->cond == BLK_ZONE_COND_FULL;
+}
+
+/* Helper function that checks if the blockgroup is fully written. */
+static inline bool znr_bg_full(struct znr_blockgroup *bg)
+{
+	/* If this is a zoned device check the zone condition */
+	if (bg->nr_zones && bg->zones)
+		return znr_bg_dev_zone_full(bg);
+
+	return bg->fs_wp >= bg->nr_sectors;
+}
+
+static inline bool znr_bg_has_wp(struct znr_blockgroup *bg)
+{
+	return bg->flags & (ZNR_BG_HAS_DEV_ZONE_WP | ZNR_BG_HAS_FS_WP);
+}
 
 int znr_bg_get_blockgroups(struct znr_blockgroup **bgs,
 			   unsigned int *nr_bgs);
