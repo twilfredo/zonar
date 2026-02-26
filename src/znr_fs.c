f0513c8d97995c7527ca491838bad1d59d5cd900
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2026-02-26 16:40:41 +1000

    fs|net: add blockgroups report api
    For filesystems that can report a writepointer for a blockgroup, this
    new function can be implemented. When supported, this function should
    iterate through all of the blockgroups as specified by nr_blockgroups
    and update the writepointer for each blockgroup.
    
    Additionally, extend the network interface for server and client to
    support the new blockgroups report over a network.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- src/znr_fs.c
+++ src/znr_fs.c
@@ -275,6 +275,21 @@ int znr_fs_get_extents_in_range(unsigned long long sector,
 						nr_sectors, ext, nr_ext);
 }
 
+int znr_fs_report_blockgroups(struct znr_bg *blockgroups,
+			      unsigned int blockgroup_no,
+			      unsigned int nr_blockgroups)
+{
+	if (znr.is_net_client)
+		return znr_net_get_blockgroup_report(&znr.ncli,
+						     blockgroups,
+						     blockgroup_no,
+						     nr_blockgroups);
+
+	return znr.mnt_dir.fs->ops->report_blockgroups(blockgroups,
+						       blockgroup_no,
+						       nr_blockgroups);
+}
+
 int znr_fs_get_blockgroups(struct znr_bg **blockgroups,
 			   unsigned int *nr_blockgroups)
 {
