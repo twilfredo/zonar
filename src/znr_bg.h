1a92310c5e2296aca1da7c10f89fc8c905cf1aef
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2026-03-12 11:01:43 +1000

    blockgroups: add a blockgroup type
    Introduce a type field to struct znr_bg. This type will represent the
    underlying zones of the blockgroup, in that it may be conventional zones
    or sequential write required zones.
    
    This replaces the use of BLK_ZONE flags in blockgroups and is used by
    the GUI to define how to render the blockgroups. A new function
    znr_bg_init_bg() is also introduced, which is used to initialize a
    blockgroup. If it has zones, the zone is used to set up the blockgroup,
    otherwise it is treated as a conventional blockgroup, analagous to
    conventional zones.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- src/znr_bg.h
+++ src/znr_bg.h
@@ -6,6 +6,14 @@
 #define ZNR_BG_H
 
 /*
+ * Block group type
+ */
+enum bg_type {
+	BG_SEQUENTIAL,
+	BG_CONVENTIONAL,
+};
+
+/*
  * Block group information.
  */
 struct znr_bg {
@@ -18,8 +26,8 @@ struct znr_bg {
 	/* Write pointer sector offset within this blockgroup */
 	unsigned long wp_sector;
 
-	/* Blockzone flags */
-	unsigned int flags;
+	/* Blockgroup type */
+	unsigned int type;
 
 	/*
 	 * Filesystem has specified a writepointer. If false, the
