265bc5a79b4ddf0d4277d1a33493a38808503279
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2026-04-01 14:48:58 +1000

    znr_bg: refactor znr_get_bg_zone_mapping()
    Split znr_get_bg_zone_mapping() into two functions,
    znr_get_bg_zone_info() which calls znr_bg_get_zone_mapping() in a loop.
    znr_bg_get_zone_mapping() will handle a single blockgroup at a time,
    this keeps the logic simple.
    
    Currently, we re-do the assigning of zone backing info to blockgroups on
    each blockgroup report, this is not needed as the zone mapping cannot
    change. Thus, add a new helper function znr_bg_get_zone_wp() and a new
    flag ZNR_BG_HAS_ZONE_INFO. This flag is set once the backing zone info
    has been populated for the respective blockgroup. In which case, only
    znr_bg_get_zone_wp() needs to be called to update the blockgroup
    writepointer.
    
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

--- src/znr_bg.h
+++ src/znr_bg.h
@@ -42,6 +42,8 @@ enum znr_bg_flags {
 	ZNR_BG_HAS_FS_WP	= (1U << 1),
 	/* Set if the blockgroup is fully written */
 	ZNR_BG_FULL		= (1U << 2),
+	/* Blockgroup has backing zone information (Zoned block devices) */
+	ZNR_BG_HAS_ZONE_INFO	= (1U << 3),
 };
 
 static inline bool znr_bg_has_wp(struct znr_bg *bg)
